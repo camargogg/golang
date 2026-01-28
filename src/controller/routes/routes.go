ad184db785c48cf80012b9869d6982247089fa36
devcamargo01 <joao.camargo@picpay.com>
2026-01-28 17:00:31 -0300

    padronização de erros e rotas criadas

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/camargogg/golang.git/src/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser/:userId", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+	
+}
